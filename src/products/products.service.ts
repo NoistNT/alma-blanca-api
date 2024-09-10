36d4d437e04c1c36f54329a643a21a2c6a17a0cc
NoistNT <arielgnr23@gmail.com>
2024-09-10 18:18:46 -0300

    Refactor in products service. Add findAllFromApi method

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,6 +1,7 @@
 import { API_URL, SESSION_NAME } from '@/constants';
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
+import { Products } from './types';
 
 interface ICookie {
   name: string;
@@ -11,38 +12,54 @@ interface ICookie {
 export class ProductsService {
   async getCookiesSession() {
     try {
-      // Check if the cookies file exists
-      if (!fs.existsSync('cookies.json')) {
-        return {
-          success: false,
-          message: 'Cookies file does not exist. Run the scraper first.',
-        };
-      }
+      // Check if cookies.json exists
+      if (!fs.existsSync('cookies.json'))
+        throw new Error('Cookies file does not exist.');
 
-      // Load cookies from the file
-      const cookies: ICookie[] = JSON.parse(
-        fs.readFileSync('cookies.json', 'utf8'),
-      );
+      // Read and parse cookies.json
+      const fileContent = fs.readFileSync('cookies.json', 'utf8');
+      if (!fileContent) throw new Error('Cookies file is empty.');
+
+      const cookies: ICookie[] = JSON.parse(fileContent);
 
       // Find the correct cookie
-      const cookieHeader = cookies.find(
-        (cookie) => cookie.name === SESSION_NAME,
+      const cookie = cookies.find(({ name }) => name === SESSION_NAME);
+      if (!cookie) throw new Error(`Cookie not found.`);
+
+      // Return cookie header
+      return { Cookie: `${cookie.name}=${cookie.value}` };
+    } catch (error) {
+      if (error instanceof Error) {
+        console.error(error.message);
+        throw new Error(error.message);
+      }
+    }
+  }
+
+  async findAllFromApi() {
+    try {
+      const headers = await this.getCookiesSession();
+      if (!headers) throw new Error('No cookies found');
+
+      const uri = `${API_URL}/v4/product?filter_page=0&filter_order=0`;
+      const response = await fetch(uri, { headers });
+      if (!response.ok)
+        throw new Error(`API request failed with status: ${response.status}`);
+
+      const data = (await response.json()) as Products[];
+
+      const products: Products[] = data.map(
+        ({ id, name, description, price, link, image }) => ({
+          id,
+          name,
+          description,
+          price,
+          link,
+          image: { id: image.id, link: image.link },
+        }),
       );
 
-      // Convert the cookie into a cookie header string
-      const headers = {
-        'Content-Type': 'application/json',
-        Cookie: `${cookieHeader.name}=${cookieHeader.value}`,
-      };
-
-      // Make a request to the API with the cookie header
-      const response = await fetch(
-        `${API_URL}/v4/product?filter_page=0&filter_order=0`,
-        { headers }, // Attach cookies to the request
-      );
-      const data = await response.json();
-
-      console.log(data);
+      return products;
     } catch (error) {
       if (error instanceof Error) {
         console.error(error.message);
