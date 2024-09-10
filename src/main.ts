f27c199712a1d8a07357c61a1aadc1b0c04e823a
NoistNT <arielgnr23@gmail.com>
2024-09-10 15:17:24 -0300

    Update main with variables from constants

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,9 @@
+import { PORT } from '@/constants';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3001);
+  await app.listen(PORT);
 }
 bootstrap();
