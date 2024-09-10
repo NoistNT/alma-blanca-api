b9342585a55fcb74465abe0c75b4e9598f758c9c
NoistNT <arielgnr23@gmail.com>
2024-09-10 15:15:24 -0300

    Add constants file

---
+++ src/constants.ts
@@ -0,0 +1,19 @@
+import { ConfigModule } from '@nestjs/config';
+
+ConfigModule.forRoot();
+
+const envVariables = ['PORT', 'BASE_URL', 'API_URL', 'SESSION_NAME'];
+
+const customError = (envVariable: string) => {
+  const message = `${envVariable} is not defined`;
+  console.error(message);
+  throw new Error(message);
+};
+
+for (const envVariable of envVariables) {
+  if (!process.env[envVariable]) {
+    customError(envVariable);
+  }
+}
+
+export const { PORT, BASE_URL, API_URL, SESSION_NAME } = process.env;
