bcc4f76f0214d6a76797ff0fb02ac78f560a398e
NoistNT <arielgnr23@gmail.com>
2024-09-10 15:17:01 -0300

    Inject imports to app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductsModule } from './products/products.module';
+import { ScraperModule } from './scraper/scraper.module';
 
 @Module({
-  imports: [],
+  imports: [ProductsModule, ScraperModule],
   controllers: [AppController],
   providers: [AppService],
 })
