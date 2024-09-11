40d0b0cd02c3e1ea37ad56e3aa1a4246176ebcf6
NoistNT <arielgnr23@gmail.com>
2024-09-11 11:08:11 -0300

    Add type annotations and update product response in products service

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,20 +1,21 @@
 import { API_URL, SESSION_NAME } from '@/constants';
 import { Injectable } from '@nestjs/common';
 import * as fs from 'fs';
-import { Products } from './types';
-
-interface ICookie {
-  name: string;
-  value: string;
-}
+import {
+  ApiResponse,
+  FindAllFromApiResponse,
+  ICookie,
+  Products,
+} from './types';
 
 @Injectable()
 export class ProductsService {
-  async getCookiesSession() {
+  private async getCookie(): Promise<{ Cookie: string }> {
     try {
       // Check if cookies.json exists
-      if (!fs.existsSync('cookies.json'))
-        throw new Error('Cookies file does not exist.');
+      if (!fs.existsSync('cookies.json')) {
+        throw new Error('Cookies file does not exist. First run the scraper.');
+      }
 
       // Read and parse cookies.json
       const fileContent = fs.readFileSync('cookies.json', 'utf8');
@@ -23,7 +24,7 @@ export class ProductsService {
       const cookies: ICookie[] = JSON.parse(fileContent);
 
       // Find the correct cookie
-      const cookie = cookies.find(({ name }) => name === SESSION_NAME);
+      const cookie: ICookie = cookies.find(({ name }) => name === SESSION_NAME);
       if (!cookie) throw new Error(`Cookie not found.`);
 
       // Return cookie header
@@ -36,30 +37,45 @@ export class ProductsService {
     }
   }
 
-  async findAllFromApi() {
+  // TODO: Update to fetch products from all pages, currently it only fetches the first page
+  async findAllFromApi(): Promise<FindAllFromApiResponse> {
     try {
-      const headers = await this.getCookiesSession();
-      if (!headers) throw new Error('No cookies found');
+      const headers = await this.getCookie();
+      if (!headers) throw new Error('Cookie not found');
 
       const uri = `${API_URL}/v4/product?filter_page=0&filter_order=0`;
+
       const response = await fetch(uri, { headers });
       if (!response.ok)
         throw new Error(`API request failed with status: ${response.status}`);
 
-      const data = (await response.json()) as Products[];
+      const { data } = (await response.json()) as ApiResponse;
+      console.log(data);
 
       const products: Products[] = data.map(
-        ({ id, name, description, price, link, images }) => ({
-          id,
-          name,
-          description,
-          price,
-          link,
-          images: images.map(({ id, link }) => ({ id, link })),
+        ({
+          idProductos,
+          p_nombre,
+          p_descripcion,
+          p_precio,
+          p_link,
+          p_oferta,
+          imagenes,
+        }) => ({
+          id: idProductos,
+          name: p_nombre,
+          description: p_descripcion,
+          price: p_precio,
+          link: p_link,
+          onSale: p_oferta,
+          images: imagenes.map(({ idImagenes, i_link }) => ({
+            id: idImagenes,
+            link: i_link,
+          })),
         }),
       );
 
-      return products;
+      return { data: products };
     } catch (error) {
       if (error instanceof Error) {
         console.error(error.message);
