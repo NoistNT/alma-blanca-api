021ca651ba45ad76e576ed6f642164c516b60ba0
NoistNT <arielgnr23@gmail.com>
2024-09-11 11:06:38 -0300

    Add types to products

--- src/products/types.ts
+++ src/products/types.ts
@@ -1,3 +1,57 @@
+export interface ICookie {
+  name: string;
+  value: string;
+}
+
+export interface ProductFromApi {
+  idProductos: number;
+  p_nombre: string;
+  p_precio: number;
+  p_link: string;
+  p_descripcion: string;
+  p_oferta: number;
+  p_precio_oferta: number;
+  p_oferta_fecha_inicio: null;
+  p_oferta_fecha: null;
+  p_tipo_venta: number;
+  p_precio_mayorista: number;
+  p_cantidad_minima: number;
+  p_destacado: number;
+  p_atributos: number;
+  p_mostrar_precio: number;
+  p_datos_stock: number;
+  p_producto_digital: number;
+  p_desactivado: number;
+  p_orden: number;
+  Categorias_idCategorias: number;
+  imagenes: [{ idImagenes: number; i_link: string }];
+  stock: [
+    {
+      idStock: number;
+      s_cantidad: number;
+      s_ilimitado: number;
+      s_precio: number;
+      s_oferta: number;
+      s_precio_oferta: number;
+      s_oferta_fecha_inicio: null;
+      s_oferta_fecha: null;
+      s_precio_mayorista: number;
+      s_cantidad_minima: number;
+      s_mostrar_precio: number;
+      s_imagen: number;
+      s_producto_digital_plazo: string;
+      s_producto_digital_observacion: string;
+      valoratributo: [];
+    },
+  ];
+}
+
+export interface ApiResponse {
+  status: number;
+  message: { code: number; description: string };
+  data: ProductFromApi[];
+}
+
 interface Images {
   id: number;
   link: string;
@@ -9,5 +63,10 @@ export interface Products {
   description: string;
   price: number;
   link: string;
+  onSale: number;
   images: Images[];
+}
+
+export interface FindAllFromApiResponse {
+  data: Products[];
 }
