635b210e9c68bd08cd5bb1d5188fa7bb836f7ed0
NoistNT <arielgnr23@gmail.com>
2024-09-11 11:26:02 -0300

    Remove scraper module, service and controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,9 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductsModule } from './products/products.module';
-import { ScraperModule } from './scraper/scraper.module';
 
 @Module({
-  imports: [ProductsModule, ScraperModule],
+  imports: [ProductsModule],
   controllers: [AppController],
   providers: [AppService],
 })
